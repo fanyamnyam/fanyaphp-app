0516bb984a823c7bf29aaaf8190aa6a73b4ad22b
fanyamnyam <fanyaloveskaveh@gmail.com>
2025-10-30 18:27:47 +0700

    Upload File DevOps

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Hello from PHP App!</h1>";
+echo "<p>Name: Syafira Tasha Herfanya</p>";
+echo "<p>NIM: 2210512073</p>";
+echo "<p>Kelas A Cloud Computing S1 SI</p>";
+echo "<p>Deployed via DevOps CI/CD pipeline</p>";
+?>
